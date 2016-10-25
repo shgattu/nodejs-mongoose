3e155101b1fde0fbadeb8504d5c349ca44da4a94
shgattu <shgattu@yahoo.com>
2016-10-25 14:27:05 -0700

    Loading individual posts

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 var home = require('./home');
 var users = require('./users');
+var posts = require('./posts');
 router.use('/',home);
 router.use('/users',users);
-
+router.use('/posts',posts);
 
 module.exports = router;
--- routes/posts.js
+++ routes/posts.js
@@ -2,7 +2,8 @@
  * Created by Anu on 10/20/2016.
  */
 var express = require('express');
-var Posts = require('../lib/post');
+var Posts = require('../lib/posts');
+var Users = require('../lib/users');
 var router = new express.Router();
 router.get('/',function(req,res){
     Posts.find(function(err,posts){
@@ -13,5 +14,16 @@ router.get('/',function(req,res){
         });
     });
 })
+router.get('/:id',function(req,res){
+    var postId = req.params.id;
+    console.log('postid is'+postId)
+    Posts.loadById(postId,function(err,post){
+        if(err) { throw err;}
+        console.log(post)
+        res.render('post',{
+            post: post
+        })
 
+    })
+})
 module.exports = router;
